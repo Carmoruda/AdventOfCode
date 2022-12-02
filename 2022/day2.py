4c8a7d76b20e748256c2906ca9ac7ab785bc85e4
Carmoruda <carmen.moreno.udaondo@gmail.com>
2022-12-02 17:21:02 +0100

    Start 2022day2

---
+++ 2022/day2.py
@@ -0,0 +1,17 @@
+def part1():
+    print(f" * Part one - Answer: {}")
+
+
+def part2():
+
+    print(f" * Part two - Answer: {}")
+
+
+def main():
+
+    print("[--- Day 2: Rock Paper Scissors ---]")
+    part1()
+    part2()
+
+
+main()
