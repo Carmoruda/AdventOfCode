059c97144d3dd327b52c92183c0912b81ac0cd3c
Carmoruda <carmen.moreno.udaondo@gmail.com>
2022-12-02 17:03:31 +0100

    Finished 2022-day1

---
+++ 2022/day1.py
@@ -0,0 +1,33 @@
+def part1(CalCount):
+    maxCal = max(CalCount)
+    print(f" * Part one - Answer: {maxCal}")
+
+
+def part2(calCount):
+    top3Sum = 0
+    for elf in range(3):
+        top3Sum += max(calCount)
+        calCount.remove(max(calCount))
+
+    print(f" * Part two - Answer: {top3Sum}")
+
+
+def main():
+    elfCalCount = []
+    currentCalCount = 0
+
+    with open("./Advent of code 2022/input/day1-input", "r") as file:
+        text = file.readlines()
+        for line in text:
+            if line == "\n":
+                elfCalCount.append(currentCalCount)
+                currentCalCount = 0
+            else:
+                currentCalCount += int(line)
+
+    print("[--- Day 1: Calorie Counting ---]")
+    part1(elfCalCount)
+    part2(elfCalCount)
+
+
+main()
